0ec284307b4384cfe443ca97fc4a407647ff89b7
chenjp <chenjp@yunheit.com>
2017-07-11 13:55:15 +0800

    init demo project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Button from 'antd/lib/button';
+import './App.css';
+import Layout from './component/Layout';
+
+class App extends Component {
+  render() {
+    return (
+      <Layout></Layout>
+    );
+  }
+}
+
+export default App;
